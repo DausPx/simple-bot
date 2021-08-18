58545a85c1fcab21d0cb6fb938cb6393b5d92106
Suad Suljovic <c.suad.suljovic@northvolt.com>
2021-08-18 20:00:38 +0200

    this

--- commands/arijan.js
+++ commands/arijan.js
@@ -8,44 +8,60 @@ module.exports = {
 	usage:false,
 	// eslint-disable-next-line no-unused-vars
 	execute(msg, args) {
-    var obim
-    var povrsina
-    var inputValue = parseInt(args[1])
-    if(args[0] == "krug") {
-        povrsina = Math.pow(inputValue,2)*Math.PI
-        obim = 2 * inputValue*Math.PI
-        msg.reply(`Povrsina je: ${povrsina}`)
-        msg.reply(`Obim je: ${obim}`)
-        
-    } 
-    else if(args[0] == "kvadrat"){
-        
-        povrsina = Math.pow(inputValue,2)
-        obim = 4 * inputValue
-        console.log(povrsina)
-        console.log(obim)
-        msg.reply(`Povrsina je: ${povrsina}`)
-        msg.reply(`Obim je: ${obim}`)
-    }  
-    else if(args[0] == "trougao"){
-        var a = inputValue
-        var b = parseInt(args[2])
-        var c = parseInt(args[3])
-        if(!(a + b > c && b + c > a && a + c > b)){
-            msg.reply("Nemoguce je formirati trougao od datih duzina")
-        }
-        obim = a + b + c
-        var s = obim / 2
-        povrsina = Math.sqrt(s * (s - a) * (s - b) * (s - c))
-        msg.reply(`Povrsina je: ${povrsina}`)
-        msg.reply(`Obim je: ${obim}`)
-    }
-    else if(args[0] == "pravougaonik"){
-        povrsina = parseInt(args[1]) * parseInt(args[2])
-        obim = 2 * (args[1] + args[2])
-        msg.reply(`Povrsina je: ${povrsina}`)
-        msg.reply(`Obim je: ${obim}`)
-    }
+        let obim
+        let povrsina
+        let geometriskoTelo = args[0]
 
+        switch(geometriskoTelo){
+            case 'krug':
+                let precnik = parseInt(args[1])
+
+                povrsina = Math.pow(precnik, 2) * Math.PI
+                obim = 2 * precnik * Math.PI
+
+                msg.reply(`Povrsina je: ${povrsina}`)
+                msg.reply(`Obim je: ${obim}`)
+            break;
+
+            case "kvadrat":
+                let stranica = parseInt(args[1])
+
+                povrsina = Math.pow(stranica, 2)
+                obim = 4 * stranica
+
+                msg.reply(`Povrsina je: ${povrsina}`)
+                msg.reply(`Obim je: ${obim}`)
+            break;
+
+            case "trougao":
+                let stranicA = parseInt(args[1])
+                let stranicaB = parseInt(args[2])
+                let stranicaC = parseInt(args[3])
+
+                let isTrougao = (stranicA + stranicaB > stranicaC && stranicaB + stranicaC > stranicA && stranicA + stranicaC > stranicaB)
+
+                if(!isTrougao){
+                    return msg.reply("Nemoguce je formirati trougao od datih duzina")
+                }
+
+                obim = a + b + c
+                var polaObima = obim / 2
+                povrsina = Math.sqrt(polaObima * (polaObima - stranicA) * (polaObima - stranicaB) * (polaObima - stranicaC))
+
+                msg.reply(`Povrsina je: ${povrsina}`)
+                msg.reply(`Obim je: ${obim}`)
+            break;
+            
+            case 'pravougaonik':
+                let stranicaA = parseInt(args[1])
+                let stranicaB = parseInt(args[2])
+
+                povrsina = stranicaA * stranicaB
+                obim = 2 * (stranicaA + stranicaB)
+
+                msg.reply(`Povrsina je: ${povrsina}`)
+                msg.reply(`Obim je: ${obim}`)
+            break;
+        }
     }
 };
