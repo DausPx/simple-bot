717c35e78ed824837a7424a9d4d56e2c7c110dca
Suad Suljovic <c.suad.suljovic@northvolt.com>
2021-08-25 20:37:54 +0200

    this class

--- commands/roll.js
+++ commands/roll.js
@@ -1,7 +1,7 @@
-let roll 
+let roll
 module.exports = {
 	name: 'roll',
-	aliases: ['rano'],
+	aliases: ['ra'],
 	description: 'Roll to win!',
 	cooldown: 2,
 	guildOnly: true,
@@ -9,7 +9,7 @@ module.exports = {
 	usage:false,
 	// eslint-disable-next-line no-unused-vars
 	execute(msg, args) {
-        
+
         if(!roll){
             roll = Math.ceil(Math.random() * 100)
             msg.reply(`You roll was: ${roll}!`)
@@ -18,7 +18,7 @@ module.exports = {
                 msg.reply(`The highest roll was: ${roll}! The game has ended`)
                 roll = undefined
             },10000)
-            
+
         }else if(roll){
             let tempRoll  = Math.ceil(Math.random() * 100)
             if(tempRoll> roll){
