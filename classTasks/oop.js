7e4f12eb6bb47c072820518dfe605b2ad9d37505
Suad Suljovic <suuad@live.com>
2021-09-13 22:34:52 +0200

    oop 2

--- classTasks/oop.js
+++ classTasks/oop.js
@@ -1,24 +1,59 @@
-function CatF(name, numOfLegs) {
-  this.name = name;
-  this.numOfLegs = numOfLegs;
+// function CatF(name, numOfLegs) {
+//   this.name = name;
+//   this.numOfLegs = numOfLegs;
+// }
+
+// CatF.prototype.mjau = function() {
+//     console.log('mjau')
+// }
+
+// CatF.prototype.greet = function() {
+//     console.log('Hello I am ' + this)
+// }
+
+// const cat1 = new CatF('suzy', 4);
+// const maybeCat2 = {name:'', numOfLegs:4}
+
+// const cat2 = new CatF();
+// const maybeCat = new Error();
+
+// console.log(cat2)
+
+class Animal {
+  constructor(name, numLegs){
+    this.name= name;
+    this.numLegs = numLegs;
+  }
+  greet(){
+    console.log(`${this.name} makes noise`)
+  }
+  goodbye(){
+    console.log(`${this.name} strarts turning`)
+  }
 }
 
-CatF.prototype.mjau = function() {
-    console.log('mjau')
+const animal1 = new Animal('milka', 4)
+
+class Dog extends Animal{
+  constructor(name, numLegs, furrColor, age){
+    super(name, numLegs)
+
+    this.furrColor = furrColor;
+    this.age = age;
+  }
+
+  greet(){
+    super.greet()
+    console.log(`${this.name} barks`)
+  }
+
+  goodbye(){
+    super.goodbye()
+    console.log(`${this.name} cryies`)
+  }
 }
 
-CatF.prototype.greet = function() {
-    console.log('Hello I am ' + this)
-}
-
-const cat1 = new CatF('suzy', 4);
-const maybeCat2 = {name:'', numOfLegs:4}
-
-const cat2 = new CatF();
-const maybeCat = new Error();
-
-console.log(cat2)
-
-
+const animal1 = new Animal('animal', 22)
+const dog1 = new Dog('rex', 4, 'gray', 5)
 
 
